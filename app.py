bab436dd2e3fb47cacd3b86528fd0f93d2a74d82
myname21-meet <myname21@meet.mit.edu>
2019-11-14 12:18:13 -0500

    Submitting flask

--- app.py
+++ app.py
@@ -6,11 +6,28 @@ app.secret_key = "MY_SUPER_SECRET_KEY"
 
 
 ##### Code here ######
+@app.route('/')
+def home():
+	return render_template("home.html")
 
+@app.route('/store')
+def store():
+	return render_template("store.html")
 
+@app.route('/cart')
+def cart():
+	return render_template("cart.html")
 
-#####################
+@app.route('/about')
+def about():
+	return render_template("about.html")
 
+@app.route('/store')
+ def store():
+ 	return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
